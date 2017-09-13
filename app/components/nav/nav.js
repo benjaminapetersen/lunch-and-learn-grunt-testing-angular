19af1a6d607f13e8c1b8b6860af8f83543505363
benjaminapetersen <admin@benjaminapetersen.me>
2017-09-13 14:01:42 -0400

    Update karma test to include component.controller

--- app/components/nav/nav.js
+++ app/components/nav/nav.js
@@ -2,12 +2,17 @@
 angular
   .module('myApp')
   .component('nav', {
-    bindings: {},
+    bindings: {
+      linkClass: '=', // not bound as a string, so must have quotes!
+      foo: '@'        // bind as a string
+    },
     templateUrl: 'components/nav/nav.html',
     controller: [
       'LINKS',
       function(LINKS) {
         this.links = LINKS;
+        // just here to prove out testing component controllers
+        this.fakeThingToTest = 'foo';
       }
     ]
   });
--- test/unit/components/nav/nav.spec.js
+++ test/unit/components/nav/nav.spec.js
@@ -3,12 +3,14 @@
 describe('nav', () => {
   let scope;
   let element;
-  let links;
+  let LINKS;
+  let componentController;
 
   beforeEach(module('myApp'));
   beforeEach(module('templates')); // due to preprocessor
-  beforeEach(inject(($rootScope, $compile, _LINKS_) => {
-    links = _LINKS_;
+  beforeEach(inject(($rootScope, $compile, _$componentController_, _LINKS_) => {
+    componentController = _$componentController_;
+    LINKS = _LINKS_;
     scope = $rootScope.$new();
     element = angular.element('<nav></nav>');
     element = $compile(element)(scope);
@@ -18,8 +20,23 @@ describe('nav', () => {
   it('should render a list of nav items based on the LINKS constant', () => {
     var navLinks = element.find('a');
     _.each(navLinks, (link, i) => {
-      expect(_.trim(link.text)).toBe(links[i].text);
+      expect(_.trim(link.text)).toBe(LINKS[i].text);
     });
+  });
+
+  it('should have a controller with a default prop', () => {
+    let bindings = {};
+    let ctrl = componentController('nav', null, bindings);
+    expect(ctrl.fakeThingToTest).toEqual('foo');
+  });
+
+  // this just tests the existence of links again, however we
+  // aren't depending on the rendering of the links, but just
+  // an assignment of an array to the controller.
+  it('should have a controller with a links property', () => {
+    let bindings = {};
+    let ctrl = componentController('nav', null, bindings);
+    expect(ctrl.links).toEqual(LINKS);
   });
 
 });
